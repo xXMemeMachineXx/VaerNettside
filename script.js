8556cfdc4af5b471d539e22f007d91c7c177ebcc
xXMemeMachineXx <101399351+xXMemeMachineXx@users.noreply.github.com>
2023-03-14 09:04:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,55 +3,34 @@ const urlAalesund = "https://api.met.no/weatherapi/nowcast/2.0/complete?lat=62.4
 const urlKristiansund = "https://api.met.no/weatherapi/nowcast/2.0/complete?lat=63.11045&lon=7.72795"
 const urlMolde = "https://api.met.no/weatherapi/nowcast/2.0/complete?lat=62.7087&lon=6.55346"
 
+var enhet = " °C"
 
 function change(temperaturVariabel) 
 {
-    useTemp = temperaturVariabel
-    update_tall(useTemp)
-    update_symbol()
-}
-
-function update_symbol(){
-    fetch(urlMolde)
-        .then(response => {
-            let data = response.json().then(data => {
-                console.log(data);
-                BildeMolde = String(data.properties.timeseries[0].data.next_1_hours.summary.symbol_code);
-                console.log("Weather:",BildeMolde);
-                document.getElementById("MoldeBilde").src="png/" + BildeMolde + ".png";
-            })
-        });
-        fetch(urlAalesund)
-        .then(response => {
-            let data = response.json().then(data => {
-                console.log(data);
-                BildeAalesund = String(data.properties.timeseries[0].data.next_1_hours.summary.symbol_code);
-                console.log("Weather:",BildeAalesund);
-                document.getElementById("AalesundBilde").src="png/" + BildeAalesund + ".png";
-            })
-        });
-        fetch(urlKristiansund)
-        .then(response => {
-            let data = response.json().then(data => {
-                console.log(data);
-                BildeKristiansund = String(data.properties.timeseries[0].data.next_1_hours.summary.symbol_code);
-                console.log("Weather:",BildeKristiansund);
-                document.getElementById("KristiansundBilde").src="png/" + BildeKristiansund + ".png";
-            })
-        });
-
+    useTemp = temperaturVariabel;
+    update_tall(useTemp);
 }
 
 function update_tall(useTemp) {
 
-    if(useTemp == 1){
     fetch(urlMolde)
         .then(response => {
             let data = response.json().then(data => {
                 console.log(data);
-                TempMolde = parseFloat(data.properties.timeseries[0].data.instant.details.air_temperature)+273.15;
-                console.log("TEMP:",TempMolde);
-                MoldeTemp.innerHTML = TempMolde.toLocaleString('en-US') + " K"
+                if(useTemp == 1){
+                    enhet = " K"
+                    TempMolde= parseFloat(data.properties.timeseries[0].data.instant.details.air_temperature)+273.15;
+                }else if(useTemp == 3){
+                    TempMolde= parseFloat((data.properties.timeseries[0].data.instant.details.air_temperature)*9/5)+32;
+                    enhet = " °F"
+                } else{
+                    TempMolde= parseFloat(data.properties.timeseries[0].data.instant.details.air_temperature)
+                    enhet = " °C"
+                }
+                MoldeTemp.innerHTML = TempMolde.toLocaleString('en-US') + enhet
+
+                BildeMolde = String(data.properties.timeseries[0].data.next_1_hours.summary.symbol_code);
+                document.getElementById("MoldeBilde").src="png/" + BildeMolde + ".png";
             })
         });
 
@@ -59,9 +38,20 @@ function update_tall(useTemp) {
         .then(response => {
             let data = response.json().then(data => {
                 console.log(data);
-                TempAalesund = parseFloat(data.properties.timeseries[0].data.instant.details.air_temperature)+273.15;
-                console.log("TEMP:",TempAalesund);
-                AalesundTemp.innerHTML = TempAalesund.toLocaleString('en-US') + " K"
+                if(useTemp == 1){
+                    enhet = " K"
+                    TempAalesund= parseFloat(data.properties.timeseries[0].data.instant.details.air_temperature)+273.15;
+                }else if(useTemp == 3){
+                    TempAalesund= parseFloat((data.properties.timeseries[0].data.instant.details.air_temperature)*9/5)+32;
+                    enhet = " °F"
+                } else{
+                    TempAalesund= parseFloat(data.properties.timeseries[0].data.instant.details.air_temperature)
+                    enhet = " °C"
+                }
+                AalesundTemp.innerHTML = TempAalesund.toLocaleString('en-US') + enhet
+
+                BildeAalesund = String(data.properties.timeseries[0].data.next_1_hours.summary.symbol_code);
+                document.getElementById("AalesundBilde").src="png/" + BildeAalesund + ".png";
             })
         });
 
@@ -69,72 +59,22 @@ function update_tall(useTemp) {
         .then(response => {
             let data = response.json().then(data => {
                 console.log(data);
-                TempKristiansund = parseFloat(data.properties.timeseries[0].data.instant.details.air_temperature)+273.15;
-                console.log("TEMP:",TempKristiansund);
-                KristiansundTemp.innerHTML = TempKristiansund.toLocaleString('en-US') + " K"
-            })
-        });
-    }else if(useTemp == 3){
-        fetch(urlMolde)
-        .then(response => {
-            let data = response.json().then(data => {
-                console.log(data);
-                TempMolde = parseFloat((data.properties.timeseries[0].data.instant.details.air_temperature)* 9/5)+32;
-                console.log("TEMP:",TempMolde);
-                MoldeTemp.innerHTML = TempMolde.toLocaleString('en-US') + " °F"
-            })
-        });
+                if(useTemp == 1){
+                    enhet = " K"
+                    TempKristiansund= parseFloat(data.properties.timeseries[0].data.instant.details.air_temperature)+273.15;
+                }else if(useTemp == 3){
+                    TempKristiansund= parseFloat((data.properties.timeseries[0].data.instant.details.air_temperature)*9/5)+32;
+                    enhet = " °F"
+                } else{
+                    TempKristiansund= parseFloat(data.properties.timeseries[0].data.instant.details.air_temperature)
+                    enhet = " °C"
+                }
+                KristiansundTemp.innerHTML = TempKristiansund.toLocaleString('en-US') + enhet
 
-        fetch(urlAalesund)
-        .then(response => {
-            let data = response.json().then(data => {
-                console.log(data);
-                TempAalesund = parseFloat((data.properties.timeseries[0].data.instant.details.air_temperature)*9/5)+32;
-                console.log("TEMP:",TempAalesund);
-                AalesundTemp.innerHTML = TempAalesund.toLocaleString('en-US') + " °F"
+                BildeKristiansund = String(data.properties.timeseries[0].data.next_1_hours.summary.symbol_code);
+                document.getElementById("KristiansundBilde").src="png/" + BildeKristiansund + ".png";
             })
         });
-
-        fetch(urlKristiansund)
-        .then(response => {
-            let data = response.json().then(data => {
-                console.log(data);
-                TempKristiansund = parseFloat((data.properties.timeseries[0].data.instant.details.air_temperature)*9/5)+32;
-                console.log("TEMP:",TempKristiansund);
-                KristiansundTemp.innerHTML = TempKristiansund.toLocaleString('en-US') + " °F"
-            })
-        });
-    } else{
-        fetch(urlMolde)
-        .then(response => {
-            let data = response.json().then(data => {
-                console.log(data);
-                TempMolde = parseFloat(data.properties.timeseries[0].data.instant.details.air_temperature);
-                console.log("TEMP:",TempMolde);
-                MoldeTemp.innerHTML = TempMolde.toLocaleString('en-US') + " °C"
-            })
-        });
-
-        fetch(urlAalesund)
-        .then(response => {
-            let data = response.json().then(data => {
-                console.log(data);
-                TempAalesund = parseFloat(data.properties.timeseries[0].data.instant.details.air_temperature);
-                console.log("TEMP:",TempAalesund);
-                AalesundTemp.innerHTML = TempAalesund.toLocaleString('en-US') + " °C"
-            })
-        });
-
-        fetch(urlKristiansund)
-        .then(response => {
-            let data = response.json().then(data => {
-                console.log(data);
-                TempKristiansund = parseFloat(data.properties.timeseries[0].data.instant.details.air_temperature);
-                console.log("TEMP:",TempKristiansund);
-                KristiansundTemp.innerHTML = TempKristiansund.toLocaleString('en-US') + " °C"
-            })
-        });
-    }
 }
 change();
 update_tall();
